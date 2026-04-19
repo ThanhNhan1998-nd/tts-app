f273edaf6931be74c7aa4ef5079d923594e4dbf2
ThanhNhan1998-nd <ltvthanhnhan18@gmail.com>
2026-04-19 12:19:45 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 from flask import Flask, render_template, request, send_file
 import edge_tts
 import asyncio
+import os
 
 app = Flask(__name__)
 
-VOICE = "vi-VN-HoaiMyNeural"
-
-async def generate_tts(text):
-    communicate = edge_tts.Communicate(text, VOICE)
+async def generate_tts(text, voice):
+    communicate = edge_tts.Communicate(text, voice)
     await communicate.save("output.mp3")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         text = request.form["text"]
-        asyncio.run(generate_tts(text))
-        return send_file("output.mp3", as_attachment=False)
+        voice = request.form["voice"]
+
+        asyncio.run(generate_tts(text, voice))
+        return send_file("output.mp3", mimetype="audio/mpeg")
+
     return render_template("index.html")
 
 if __name__ == "__main__":
-    import os
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
